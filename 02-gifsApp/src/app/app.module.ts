8551636f7094176ea58a29b47baa1369e8cfc227
unknown <formacio@PC-1062606.formacio.local>
2022-05-10 13:03:47 +0200

    y

--- 02-gifsApp/src/app/app.module.ts
+++ 02-gifsApp/src/app/app.module.ts
@@ -1,10 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +16,7 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
     
--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,32 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { HttpClient, JsonpClientBackend } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interface/gif.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private APIkey:string='weqa6HY5uwjuLYZyAFk5x9mevNMNEDM7'
+
   private _historial:string[]=[]
+
+  //TODO: Cambiar por el tipo correcto
+  public resultado:Gif[]=[]
 
   get historial():string[]{
     return [...this._historial]
   }
+
+  constructor(private http:HttpClient) {
+
+      this._historial=JSON.parse(localStorage.getItem('historial')!)  || []
+    
+
+  }
+    
+  
 
   agregarGifs(query:string){
 
@@ -21,5 +37,16 @@ export class GifsService {
     this._historial.unshift(query)
     console.log(this._historial)
     }
+
+    localStorage.setItem('historial',JSON.stringify(this._historial))
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=weqa6HY5uwjuLYZyAFk5x9mevNMNEDM7&q=${query}&limit=10`)
+    .subscribe((resp)=>{
+      console.log(resp.data)
+      this.resultado=resp.data
+      
+    })
+   
+
   }
 }
